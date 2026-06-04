81b4856957a14d0f0715f87328584a784dd44e30
prathameshdeogirkar <166210279+prathameshdeogirkar@users.noreply.github.com>
2026-06-04 21:37:15 +0530

    initial commit

--- config/db.js
+++ config/db.js
@@ -1,10 +1,12 @@
+require("dotenv").config();
+
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "real_estate",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 db.connect((err) => {
